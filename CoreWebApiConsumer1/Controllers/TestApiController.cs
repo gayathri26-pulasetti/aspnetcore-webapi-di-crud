fb7fc2017a475385b94d81b07c07a2ed669bb704
suresh721 <psureshbabu284@gmail.com>
2026-02-27 23:13:28 +0530

    CoreWebApiConsumer1 - jQuery Ajax

---
+++ CoreWebApiConsumer1/Controllers/TestApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApiConsumer1.Controllers
+{
+    public class TestApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
