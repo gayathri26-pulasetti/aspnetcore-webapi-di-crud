86de03f1ff135a00115e37faa7305c7a3a594fea
suresh721 <psureshbabu284@gmail.com>
2026-02-28 14:55:56 +0530

    CoreWebApiConsumer2 - C# HttpClient

---
+++ CoreWebApiConsumer2/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreWebApiConsumer2.Models
+{
+    public class Customer
+    {
+        public int Custid { get; set; }
+        public string? Name { get; set; }
+        public decimal? Balance { get; set; }
+        public string? City { get; set; }
+        public bool Status { get; set; }
+
+
+    }
+}
